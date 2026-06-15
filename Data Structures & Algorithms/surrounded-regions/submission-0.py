e519afa88607b5286c2fb78ee0cf734990eb6898
Arshdeep Jassi <97292001+a-jassi@users.noreply.github.com>
2026-06-15 04:54:03 -0700

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        notSurrounded = set()
+
+        def dfs(r, c):
+            if r < 0 or r >= ROWS:
+                return
+            if c < 0 or c >= COLS:
+                return
+            if (r, c) in notSurrounded or board[r][c] == 'X':
+                return
+            
+            notSurrounded.add((r, c))
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        for r in range(ROWS):
+            dfs(r, 0)
+            dfs(r, COLS - 1)
+
+        for c in range(COLS):
+            dfs(0, c)
+            dfs(ROWS - 1, c)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) not in notSurrounded:
+                    board[r][c] = "X"
